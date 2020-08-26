89e19ee8f45008307588ea9692c363c214b4529d
Sean Ni <nixiang915@hotmail.com>
2020-08-26 12:14:05 +0000

    stdin added

--- play.js
+++ play.js
@@ -1,4 +1,19 @@
 const {connect} = require('./client');
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+};
+
+const stdin =setupInput();
 console.log('Connecting ...');
 connect();
+stdin.on('data', (key) => {
+  if (key === '\u0003') {
+    process.stdout.write('Thanks for using me, ciao!\n');
+    process.exit();
+  }
+});
